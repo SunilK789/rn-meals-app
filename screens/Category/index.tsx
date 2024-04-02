6e63873c9ff309efd6e65e254a6fa8c6d189eb3f
Sunil Kumar <sunilpalarya@gmail.com>
2024-04-02 11:41:09 +0530

    feature: added main screen

---
+++ screens/Category/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text, FlatList, ListRenderItemInfo } from "react-native";
+import React from "react";
+import { CATEGORIES } from "../../data/dummy-data";
+import CategoryGridTile from "../../components/CategoryGridTile";
+import Category from "../../models/category";
+
+const renderedCategoryItem = (item: ListRenderItemInfo<Category>) => {
+  return (
+    <>
+      <CategoryGridTile title={item.item.title} color={item.item.color} />
+    </>
+  );
+};
+
+const CategoriesScreen = () => {
+    console.log("CATEGORIES===>", CATEGORIES);
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={(item) => renderedCategoryItem(item)}
+    />
+  );
+};
+
+export default CategoriesScreen;
